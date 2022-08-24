3520fe27b2a25422fb959a006f5b5441caf6955c
wuwenx <376679129@qq.com>
2022-08-24 16:56:24 +0800

    fix: txt

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    dasd 
+      asd asd sd 
+    asd 
     <App />
   </React.StrictMode>
 );
